66280cfd5b1f134fc0f3d86e77f9e2b1a3f57e64
hddudu <15574948314@163.com>
2019-08-04 22:48:33 +0800

    手写： mybatis ： v1版本 v2版本待完成

---
+++ SpringORMDemo/MeBatis2/src/main/java/com/hongdu/gupao/mebatis/v2/MeBatisV2Test.java
@@ -0,0 +1,15 @@
+package com.hongdu.gupao.mebatis.v2;
+
+/**
+ * @ClassName MeBatisV2Test
+ * @Description TODO
+ * @Author dudu
+ * @Date 2019/8/4 20:04
+ * @Version 1.0
+ */
+public class MeBatisV2Test {
+
+    public static void main(String[] args) {
+        //TODO : v2版本的mybatis手写待完成
+    }
+}
