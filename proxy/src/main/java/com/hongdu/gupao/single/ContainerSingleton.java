3bc8a267b65320cc1fad692c81360c4e204feba4
hddudu <15574948314@163.com>
2020-02-27 16:55:34 +0800

    加锁

--- proxy/src/main/java/com/hongdu/gupao/single/ContainerSingleton.java
+++ proxy/src/main/java/com/hongdu/gupao/single/ContainerSingleton.java
@@ -16,39 +16,64 @@ public class ContainerSingleton {
     }
     private static Map<String,Object> ioc = new ConcurrentHashMap<String, Object>();
     // 多线程会 访问到这里来的时候， 又会出现覆盖实例的问题
-    static volatile Object instance = null;
+//    static volatile Object instance = null;
     public static Object getInstance(String className){
+//    public synchronized static Object getInstance(String className){
+        //方式一 ： 加锁到方法 ： 阻塞严重 : 等待不要重复执行的代码块比较大 ： 花费时间多: 不采用
 
         //这个地方会有线程安全问题：
 //        synchronized (singleton) {// 不能加到外面 ： 性能损耗大
-        if(instance == null ) {
-            synchronized (singleton) {
-                if(instance == null) {
-                    try {
-                        instance = Class.forName(className).newInstance();
-                        ioc.put(className, instance);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-        return instance;
-
-//            if(!ioc.containsKey(className)){
-//                synchronized (singleton) {
+        //这个写法不对 ： 经过一次 CLassName后， Instance一定有值了， 那返回的永远是容器中注册的唯一的一个实例对象
+//        if(instance == null ) {
+//            synchronized (singleton) {
+//                if(instance == null) {
 //                    try {
 //                        instance = Class.forName(className).newInstance();
 //                        ioc.put(className, instance);
-//                    }catch (Exception e){
+//                    } catch (Exception e) {
 //                        e.printStackTrace();
 //                    }
-//                    return instance;
 //                }
+//            }
+//        }
+//        return instance;
+
+        Object instance = null;
+        //包含大代码块 : 方式二： 仍然感觉不太好 : 比锁整个方法好不了多少
+//        synchronized (singleton) {
+//            if(!ioc.containsKey(className)){
+//                try {
+//                    instance = Class.forName(className).newInstance();
+//                    ioc.put(className, instance);
+//                }catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//                return instance;
 //            }else{
 //                return ioc.get(className);
 //            }
 //        }
-    }
 
+
+        if(!ioc.containsKey(className)){
+            try {
+                instance = Class.forName(className).newInstance();
+                //方式三： 感觉稍微好点 ： 还是不是很爽
+                synchronized (singleton) {
+                    if(ioc.containsKey(className)) {
+                        return ioc.get(className);
+                    } else {
+                        ioc.put(className, instance);
+                    }
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return instance;
+        }else{
+            return ioc.get(className);
+        }
+
+    }
 }
