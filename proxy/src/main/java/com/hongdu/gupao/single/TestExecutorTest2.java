f9abdbaf40b2ceea5c977689b3b3a103633568f2
hddudu <15574948314@163.com>
2020-02-27 17:29:20 +0800

    测试 可以通过

---
+++ proxy/src/main/java/com/hongdu/gupao/single/TestExecutorTest2.java
@@ -0,0 +1,20 @@
+package com.hongdu.gupao.single;
+
+/**
+ * @ClassName TestExecutorTest
+ * @Description 测试容器式单例的安全问题 ： 确实存在
+ * @Author dudu
+ * @Date 2020/2/27 17:16
+ * @Version 1.0
+ */
+public class TestExecutorTest2 {
+    public static void main(String[] args) {
+        ConcurrentExecutor concurrentExecutor = new ConcurrentExecutor();
+        try {
+            concurrentExecutor.execute(new ContainerExecutorRunHandler(),
+                   10000, 100 );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
